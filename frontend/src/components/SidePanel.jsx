334f7cefe97a59fd4b99d2b1546f73aae33411ef
Mehul Gupta <mehulguptar.w453@gmail.com>
2025-05-23 18:47:15 +0530

    `Refactored SidePanel component to use a single background image and updated styling`

--- frontend/src/components/SidePanel.jsx
+++ frontend/src/components/SidePanel.jsx
@@ -1,46 +1,32 @@
 import React from "react";
-import logoBanner from "../assets/images/loginBanner.png";
 import logo from "../assets/images/logo.png";
-import vector1 from "../assets/images/Vector1.png";
-import vector2 from "../assets/images/Vector2.png";
-import vector3 from "../assets/images/Vector3.png";
-import vector from "../assets/images/Vector.png";
+import vector from "../assets/images/Vector.png"; // background image
 
 const SidePanel = () => {
   return (
-    <div className="w-full bg-gray-100 relative flex justify-center items-center">
-      {/* Vectors */}
-      <img
-        src={vector1}
-        alt="Vector Top Left"
-        className="absolute top-0 left-0 w-50 h-60"
-      />
-      <img
-        src={vector2}
-        alt="Vector Bottom Right"
-        className="absolute bottom-0 right-0 w-50 h-60"
-      />
-      <img
-        src={vector}
-        alt="Vector Bottom Right"
-        className="absolute top-0 right-0 w-40 h-30"
-      />
+    <div
+      className="w-full h-full relative flex justify-center items-center bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${vector})`,
+      }}
+    >
+      {/* Overlay Layer */}
+      <div className="absolute inset-0 bg-black opacity-55 z-0" />
 
       {/* Banner Content */}
-      <div className="text-center relative">
-        <img
-          src={vector3}
-          alt="Vector Bottom Right"
-          className="absolute top-20 right-0 w-30 h-30"
-        />
+      <div className="text-center relative z-10 p-4">
         <img
           src={logo}
           alt="Banner"
-          className="w-[500px] h-[400px] mx-auto"
+          className="w-[500px] h-[400px] mx-auto bg-white"
         />
-        <h2 className="text-4xl font-bold mt-4">Shanti Devi Health Care Hospital</h2>
-        <p className="text-gray-600 mt-2 font-semibold">
-         G.F Shop No.: 3, Sun South Trade, Opp. Bopal Police Station, Gala Gym Khana Road, <br /> Bopal, Ahmedabad - 380058, India
+        <h2 className="text-4xl font-bold mt-4 text-white">
+          Shanti Devi Health Care Hospital
+        </h2>
+        <p className="text-gray-200 mt-2 font-semibold">
+          G.F Shop No.: 3, Sun South Trade, Opp. Bopal Police Station, Gala Gym Khana Road,
+          <br />
+          Bopal, Ahmedabad - 380058, India
         </p>
       </div>
     </div>
